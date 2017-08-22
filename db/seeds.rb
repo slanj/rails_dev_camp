3488cd4153a57aa52507ed610aa902b5e93441e8
Slanj <slanjr@gmail.com>
2017-08-22 11:56:09 +0000

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+  Blog.create!(
+    title: "Awesome blog post #{blog + 1}"  
+  )
+end
+
+puts 'Creation accomplished: 10 blog posts'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill + 1}",
+    percent_utilized: 15
+  )
+end
+
+puts 'Creation accomplished: 5 skills'
+
+9.times do |item|
+  Portfolio.create!(
+    title: "Portfolio title #{item}",
+    subtitle: "My super app",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis dui eget lacus tincidunt luctus. Nunc vel nisl nunc. Mauris tristique posuere neque at lacinia. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam sapien tellus, placerat quis fringilla non, fermentum sit amet augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet maximus bibendum. Quisque gravida turpis feugiat turpis vulputate lacinia. Aliquam a porta felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    main_image: 'https://unsplash.it/600/400?random',
+    thumb_image: 'https://unsplash.it/300?random'
+  )
+end
+
+puts 'Creation accomplished: 9 portfolio items'

c4b14181d10c6f3708d7fbb09584c41e42ddd28c
Slanj <slanjr@gmail.com>
2017-08-30 10:12:19 +0000

    Custom routes names

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :articles
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
   
   root to: 'pages#home'
   

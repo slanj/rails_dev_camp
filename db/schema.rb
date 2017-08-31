87a7e7e1dae34dc3a85a297c921cd0a6bb0a1ab4
Slanj <slanjr@gmail.com>
2017-08-31 08:18:56 +0000

    Blog status with enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830104848) do
+ActiveRecord::Schema.define(version: 20170831075527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170830104848) do
     t.datetime "updated_at", null: false
     t.string "body"
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
